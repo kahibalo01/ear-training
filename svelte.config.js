ebba10e80904009cc4916b60a49cd37dd28734a2
kahibalo <kahibalo01@gmail.com>
2024-12-07 01:47:30 +0800

    Test for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		appDir: 'app',
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/ear-training' : '/ear-training'
-		}
+		adapter: adapter()
 	}
 };
 
